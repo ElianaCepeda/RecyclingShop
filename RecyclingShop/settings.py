478bba51b71721b9278605672ea15c4253d6c38c
ElianaCepeda <90657249+ElianaCepeda@users.noreply.github.com>
2021-10-05 19:33:51 -0500

    Actualizando conexion a base de datos

--- RecyclingShop/settings.py
+++ RecyclingShop/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'RecyclingShop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'recyclingshopbd',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
