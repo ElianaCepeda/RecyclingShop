4eb01624e5556835c72575697c3663f9e96ad12b
Eliana Cepeda <eli.cepedagonzalez@gmail.com>
2021-10-05 19:48:31 -0500

    Actualizando vista y dump file

--- apps/registro/views.py
+++ apps/registro/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    salida = "<html>"
+    salida = salida + "<body>"
+    salida = salida + "<h1>Bienvenido al modulo de registro de RecyclingShop</h1>"
+    salida = salida + "<form>"
+    salida = salida + "<p>Diligencie sus usuario: <input></input></p>"
+    salida = salida + "<p>Diligencie sus password: <input /></p>"
+    salida = salida + "<p><input type='submit'></p>"
+    salida = salida + "</form>"
+    salida = salida + "</body>"
+    salida = salida + "</html>"
+    return HttpResponse(salida)
 
 # Create your views here.
