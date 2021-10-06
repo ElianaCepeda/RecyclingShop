d5de51551cffd93dbcab9f80a5c16415cce3ad5d
ElianaCepeda <90657249+ElianaCepeda@users.noreply.github.com>
2021-10-05 19:31:29 -0500

    Actualizando conexion a la base de datos

--- RecyclingShop/settings.py
+++ RecyclingShop/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'RecyclingShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
